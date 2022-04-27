19e1eb8ecae34098d9cd45b54d2ded4e359537a8
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2022-04-27 17:58:40 +0200

    Asset Mapping Tests

--- CthulhuWizard/CthulhuWizard.Tests.Shared/AssetGenerator.cs
+++ CthulhuWizard/CthulhuWizard.Tests.Shared/AssetGenerator.cs
@@ -7,8 +7,8 @@ public static class AssetGenerator {
 	public static Asset Asset { get; set; }
 	static AssetGenerator() {
 		Asset = new Faker<Asset>()
-		        .RuleFor(a => a.Assets, f => f.Finance.Amount())
-		        .RuleFor(a => a.Cash, f => f.Finance.Amount())
-		        .RuleFor(a => a.SpendingLevel, f => f.Finance.Amount());
+		        .RuleFor(a => a.Assets, f => Convert.ToInt32(f.Finance.Amount()))
+		        .RuleFor(a => a.Cash, f => Convert.ToInt32(f.Finance.Amount()))
+		        .RuleFor(a => a.SpendingLevel, f => Convert.ToInt32(f.Finance.Amount()));
 	}
 }

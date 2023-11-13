c63ea088c069a1b8769e07f9e45a29fa7811c7fc
GitHub Action <ACTION@github.com>
2023-11-13 14:15:36 +0100

    remove comment

--- CthulhuWizard/CthulhuWizard.Tests.Integrations/WebApplicationFactory.cs
+++ CthulhuWizard/CthulhuWizard.Tests.Integrations/WebApplicationFactory.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 namespace CthulhuWiard.Tests.Integrations;
 
 public class WebApplicationFactory : WebApplicationFactory<Startup> {
-    // podmień mój RavenDbContext na TestRavenDbContext
     protected override void ConfigureWebHost(IWebHostBuilder builder) {
         builder.ConfigureServices(services => {
             var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IRavenDbContext));

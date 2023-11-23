087e4b446318c9b1148d5d2cfc44794aa82dce5e
Katarzyna-Kadziolka <62292047+Katarzyna-Kadziolka@users.noreply.github.com>
2023-11-23 13:44:04 +0100

    Add filtering by type (#10)
    Co-authored-by: GitHub Action <ACTION@github.com>

--- CthulhuWizard/CthulhuWizard.Application/Requests/Equipments/Queries/GetEquipments/GetEquipmentsQuery.cs
+++ CthulhuWizard/CthulhuWizard.Application/Requests/Equipments/Queries/GetEquipments/GetEquipmentsQuery.cs
@@ -4,5 +4,7 @@ namespace CthulhuWizard.Application.Requests.Equipments.Queries.GetEquipments;
 
 public class GetEquipmentsQuery : IQuery<List<EquipmentDto>> {
 	public string? Name { get; set; }
-	public double? Price { get; set; } = null;
+	public double? Price { get; set; }
+	public string? Type { get; set; }
+
 }
--- CthulhuWizard/CthulhuWizard.Tests.Unit/HandlersTests/Equipments/Queries/GetEquipmentsQueryHandlerTests.cs
+++ CthulhuWizard/CthulhuWizard.Tests.Unit/HandlersTests/Equipments/Queries/GetEquipmentsQueryHandlerTests.cs
@@ -65,7 +65,7 @@ public class GetEquipmentsQueryHandlerTests {
                 Name = "AB",
                 Type = "Test type",
                 Price = 5
-            },
+            }
         };
         var request = new GetEquipmentsQuery();
         request.Name = "Test";
@@ -84,8 +84,8 @@ public class GetEquipmentsQueryHandlerTests {
         var expectedEquipments = TestMapper.Instance.Map<List<EquipmentDto>>(equipmentsFromDb);
 
         // Act
-        var result = await handler.Handle(request, CancellationToken.None);
         testDb.WaitForIndexing();
+        var result = await handler.Handle(request, CancellationToken.None);
         //Assert
         result.Should().HaveSameCount(expectedEquipments);
         result.Should().BeEquivalentTo(expectedEquipments);
@@ -140,6 +140,129 @@ public class GetEquipmentsQueryHandlerTests {
         // Act
         var result = await handler.Handle(request, CancellationToken.None);
         testDb.WaitForIndexing();
+        //Assert
+        result.Should().HaveSameCount(expectedEquipments);
+        result.Should().BeEquivalentTo(expectedEquipments);
+    }
+    
+    [Test]
+    public async Task Handle_Type_ShouldReturnEquipmentsDtoListWithGivenType() {
+        // Arrange
+        using var testDb = new RavenTestDb();
+        var seededEquipments = new List<EquipmentEntity> {
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Test",
+                Type = "Test type A",
+                Price = 1
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "ATest",
+                Type = "Test type A",
+                Price = 2
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "TestB",
+                Type = "Test type",
+                Price = 3
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "ATestB",
+                Type = "Test type",
+                Price = 4
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "AB",
+                Type = "Test type",
+                Price = 5
+            },
+        };
+        var request = new GetEquipmentsQuery();
+        request.Type = "Test type A";
+        var handler = new GetEquipmentsQueryHandler(testDb, TestMapper.Instance);
+        
+        using var session = testDb.Store.OpenSession();
+        foreach (var equipment in seededEquipments) {
+            session.Store(equipment); 
+        }
+        session.SaveChanges();
+        
+        var equipmentsFromDb = session
+            .Query<EquipmentEntity>()
+            .Where(a => a.Type == request.Type)
+            .ToList();
+        var expectedEquipments = TestMapper.Instance.Map<List<EquipmentDto>>(equipmentsFromDb);
+
+        // Act
+        testDb.WaitForIndexing();
+        var result = await handler.Handle(request, CancellationToken.None);
+        //Assert
+        result.Should().HaveSameCount(expectedEquipments);
+        result.Should().BeEquivalentTo(expectedEquipments);
+    }
+    [Test]
+    public async Task Handle_AllFilters_ShouldReturnEquipmentsDtoListWithGivenConditions() {
+        // Arrange
+        using var testDb = new RavenTestDb();
+        var seededEquipments = new List<EquipmentEntity> {
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Test",
+                Type = "Test type",
+                Price = 1
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "ATest",
+                Type = "c",
+                Price = 2
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "ATest",
+                Type = "Test type A",
+                Price = 3
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "ATest",
+                Type = "Test type",
+                Price = 4
+            },
+            new() {
+                Id = Guid.NewGuid().ToString(),
+                Name = "AB",
+                Type = "Test type",
+                Price = 5
+            }
+        };
+        var request = new GetEquipmentsQuery {
+            Name = "ATest",
+            Price = 2,
+            Type = "ATest"
+        };
+        var handler = new GetEquipmentsQueryHandler(testDb, TestMapper.Instance);
+        
+        using var session = testDb.Store.OpenSession();
+        foreach (var equipment in seededEquipments) {
+            session.Store(equipment); 
+        }
+        session.SaveChanges();
+        
+        var equipmentsFromDb = session
+            .Query<EquipmentEntity>()
+            .Search(a => a.Name, $"*{request.Name}*" )
+            .Where(a => a.Type == request.Type && a.Price <= request.Price )
+            .ToList();
+        var expectedEquipments = TestMapper.Instance.Map<List<EquipmentDto>>(equipmentsFromDb);
+
+        // Act
+        testDb.WaitForIndexing();
+        var result = await handler.Handle(request, CancellationToken.None);
         //Assert
         result.Should().HaveSameCount(expectedEquipments);
         result.Should().BeEquivalentTo(expectedEquipments);
